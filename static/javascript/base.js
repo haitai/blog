510e98f1e2022b69ca7d4ade38753f8d4a15e940
haitai <github@huhaitai.com>
2018-01-19 16:13:49 +0800

    19/1/18

--- static/javascript/base.js
+++ static/javascript/base.js
@@ -36,42 +36,9 @@ $(document).ready(function() {
         $("textarea")[0].focus();
 
     });
-    var b=jQuery("#yui-main > .yui-b"),
-    d=jQuery("#sidebar");
-    if(b.length){
-        var e=jQuery('<div id="collapsible-arrow">'),
-        i=jQuery('<div id="collapsible">'),
-        k=function(o){
-            var p=d.offset(),eo=p.left+12;
-            if(d.is(":hidden")){
-                e.css({left:"auto",right:"12px",top:(o.pageY||o.clientY)+"px"})}
-            else
-                e.css({left:eo+"px",top:(o.pageY||o.clientY)+"px"})};
-
-        b.css("position","relative").append(i);
-        jQuery(document.body).append(e);
-        jQuery.browser.msie&&jQuery.browser.version<7&&b.css("right","-5px");
-        b.is(".skip-collapsible")||i.mouseover(function(o){
-            i.addClass("hover");
-            if(d.is(":hidden")){
-                i.css("right","0");e.addClass("collapsed")}
-            else e.removeClass("collapsed");
-                e.show();k(o)})
-        .mousemove(function(o){k(o)}).mouseout(function(){
-            i.removeClass("hover");
-            i.css("right","");
-            e.hide()}).click(function(){
-                e.hide();i.removeClass("hover").css("right","");d.toggle();
-                if(d.is(":visible")){
-                    b.css("margin-right","24.0769em");
-                    jQuery.browser.msie&&jQuery.browser.version<7&&b.css("right","");
-                    }
-                else{
-                    b.css("margin-right","0");
-                    jQuery.browser.msie&&jQuery.browser.version<7&&b.css("right","")}});
-        /*i.height("100%"b.height())*/}
 
 });
+
 function toggle(button) {
     if (button.className == "open-button") {
         jQuery(button).parent().next().slideUp(function() {
